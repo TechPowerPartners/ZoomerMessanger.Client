8204f68f465abe40c94f0a2358396648ac8049a8
Ferchke7 <ferdavs21@gmail.com>
2024-02-14 01:09:15 +0900

    added localization for korean english and russian

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_localizations/flutter_localizations.dart';
 
 void main() {
   runApp(const MyApp());
@@ -12,7 +13,59 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return const MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: Text("Test"),
+      localizationsDelegates: [
+        GlobalMaterialLocalizations.delegate,
+        GlobalWidgetsLocalizations.delegate,
+        GlobalCupertinoLocalizations.delegate
+      ],
+      supportedLocales: [
+        Locale('en', ''),
+        Locale('ru', ''),
+        Locale('kr', ''),
+      ],
+      home: TempWidget(),
+    );
+  }
+}
+
+class TempWidget extends StatefulWidget {
+  const TempWidget({super.key});
+
+  @override
+  State<TempWidget> createState() => _TempWidgetState();
+}
+
+class _TempWidgetState extends State<TempWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            Row(
+              children: <Widget>[
+                Text("1"),
+                Text("2"),
+                Text("3"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Text("1"),
+                Text("2"),
+                Text("3"),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Text("1"),
+                Text("2"),
+                Text("3"),
+              ],
+            )
+          ],
+        ),
+      ),
     );
   }
 }
---
+++ lib/remote/request.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class DodikWidget extends StatefulWidget {
+  const DodikWidget({super.key});
+
+  @override
+  State<DodikWidget> createState() => _DodikWidgetState();
+}
+
+class _DodikWidgetState extends State<DodikWidget> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
