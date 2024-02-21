bf8db338b2e05238b961a80adceb88a7d0d3c8fd
Ferchke17 <ferdavs@naver.com>
2024-02-21 18:36:41 +0900

    fixed token null

--- lib/global/global.dart
+++ lib/global/global.dart
@@ -2,6 +2,7 @@ import 'package:zoomerm_client/helpers/login_helper.dart';
 import 'package:zoomerm_client/models/login_model.dart';
 
 getGlobal() async {
+  
   var login = await LocalHelper.getAccountFromLocal();
   currentLogin = login;
 }
